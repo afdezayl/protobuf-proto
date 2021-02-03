bfdcb5e829f5f85a4bb271b5a44437ff23d7cbde
afdezayl <45856805+afdezayl@users.noreply.github.com>
2021-02-03 16:36:40 +0100

    enable strict typescript checking

--- libs/shared/proto/src/lib/custom-message.ts
+++ libs/shared/proto/src/lib/custom-message.ts
@@ -2,5 +2,5 @@ import { Field, Message } from 'protobufjs/light';
 
 export class CustomMessage extends Message<CustomMessage> {
   @Field.d(1, 'string', 'required')
-  text: string;
+  text!: string;
 }
